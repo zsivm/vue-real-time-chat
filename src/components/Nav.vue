c16eb17faf58bfb355e568869a79e6791db42b9e
Mihael Zsivics <zsivics.mihael@gmail.com>
2021-07-04 15:54:21 +0200

    fixed nav vue file

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,20 @@
 <template>
-    <nav>
-        <div>
-            <h1><strong>Vue</strong> Chat App</h1>
+  <nav>
+    <div>
+      <h1><strong>Vue</strong> Chat App</h1>
 
-            <div v-if="isLogin" class="login">
-                <Avatar :src="user.photoURL" />
-                <button @click="signOut" class="text-gray-400 hover:text-white">
-                    Sign Out
-                </button>
-            </div>
-            
-            <button v-else @click="signIn" class="bg-green-500 hover:bg-green-600">
-                Sign In
-            </button>
-        </div>
-    </nav>
+      <div v-if="isLogin" class="login">
+        <Avatar :src="user.photoURL" />
+        <button class="text-gray-400 hover:text-white" @click="signOut">
+          Sign Out
+        </button>
+      </div>
+
+      <button v-else class="bg-green-500 hover:bg-green-600" @click="signIn">
+        Sign in
+      </button>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -25,7 +25,8 @@ export default {
     components: { Avatar },
     setup() {
         const { user, isLogin, signOut, signIn } = useAuth();
-        return (user, isLogin, signOut, signIn);
+        return { user, isLogin, signOut, signIn };
     }
+
 }
 </script>
